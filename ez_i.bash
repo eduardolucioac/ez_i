f22337a14ff30737ecc36d776fbc7d1da4291d0a
Eduardo Lúcio Amorim Costa <eduardolucioac@gmail.com>
2021-04-01 19:17:45 -0300

    Update ez_i.bash

--- ez_i.bash
+++ ez_i.bash
@@ -28,14 +28,21 @@ C_PRINT_LONG_INSTRUCTIONS="
 [ CONTINUE: q ]
 "
 
+F_PRINT_LONG_STR_R=0
 f_print_long_str() {
-    : 'Paginar entradas de texto quando forem maior que o tamanho do terminal.
+    : 'Paging text entries when they are larger than the terminal size.
 
-    Se a entrada de texto for maior do que o tamanho atual do terminal então 
-    pagina a mesma.
+    If the text entry is larger than the current size of the terminal then it will 
+    page it.
 
     Args:
-        STR_INPUT_P (str): Texto a ser exibido.
+        STR_INPUT_P (str): Text to display.
+
+    Returns:
+        F_PRINT_LONG_STR_R (int): 0 - If is NOT a string (STR_INPUT_P) that needs 
+    be paged; 1 - If is a string  (STR_INPUT_P) that needs be paged. NOTE: Useful 
+    to control the execution of your script and allow you control the flow of printed 
+    information on terminal.
     '
 
     STR_INPUT_P=$1
@@ -55,12 +62,14 @@ f_print_long_str() {
     if [ ${STR_LENGTH} -gt ${CON_SIZE} ] || [ ${STR_LINES} -gt $(($CON_ROWS - 1)) ] ; then
         echo -n "$C_PRINT_LONG_INSTRUCTIONS$STR_INPUT_P" | less -F
 
-        # NOTE: Serve para manter na tela do terminal o último conteúdo 
-        # printado! By Questor
-        echo -n "$C_PRINT_LONG_INSTRUCTIONS$STR_INPUT_P"
+        # NOTE: Keep the last printed content on the terminal screen. By Questor
+        echo "$C_PRINT_LONG_INSTRUCTIONS$STR_INPUT_P"
+
+        F_PRINT_LONG_STR_R=1
 
     else
-        echo -n "$STR_INPUT_P"
+        echo "$STR_INPUT_P"
+        F_PRINT_LONG_STR_R=0
     fi
 
 }
@@ -85,6 +94,76 @@ f_enter_to_cont() {
     fi
 
     read -p "Press enter to continue..." nothing
+}
+
+F_EZ_READ_R=""
+f_ez_read() {
+    : 'Facilitar o uso do comando "read".
+
+    Trata algumas limitações do comando "read".
+
+    Args:
+        QUESTION_P (str): Pergunta a ser feita ao usuário.
+        IS_PWD_P (Optional[int]): 0 - O input NÃO é um password; 1 - O input é um 
+    password. Padrão 0.
+        WAIT_UNTIL_P (Optional[int]): Esperar até o intervalo informado 
+            (em segundos). Caso seja vazio irá esperar uma resposta indefinidamente.
+        WAIT_UNTIL_RTN_P (Optional[str]): Valor a ser assumido após o intervalo 
+            em WAIT_UNTIL_P.
+
+    Returns:
+        F_EZ_READ_R (str): Entrada digitada pelo usuário.
+    '
+
+    QUESTION_P=$1
+    IS_PWD_P=$2
+    if [ -z "$IS_PWD_P" ] ; then
+        IS_PWD_P=0
+    fi
+    WAIT_UNTIL_P=$3
+    WAIT_UNTIL_RTN_P=$4
+
+    F_EZ_READ_R=""
+
+    READ_PARAMS=""
+    if [ ${IS_PWD_P} -eq 1 ] ; then
+    # NOTE: Habilita o "modo password". By Questor
+
+        READ_PARAMS="-s "
+    fi
+    if [ -n "$WAIT_UNTIL_P" ] ; then
+    # NOTE: Habilita o "timeout". By Questor
+
+        READ_PARAMS="$READ_PARAMS-t$WAIT_UNTIL_P "
+        QUESTION_P="$QUESTION_P (\"$WAIT_UNTIL_RTN_P\" in $WAIT_UNTIL_P seconds) "
+    fi
+
+    # NOTE: Exibimos de forma separada o conteúdo de "QUESTION_P" usando o comando
+    # "echo" e não usamos o comando "read" para exibi-lo porque "strings" muito longas
+    # ocasionam o erro "Segmentation fault (core dumped)" na execução do comando
+    # "read" além de outros problemas de funcionamento. By Questor
+    echo -n "$QUESTION_P"
+    eval "read -e $READ_PARAMS-r F_EZ_READ_R"
+
+    # NOTE: Os "echo" abaixo serão necessários se a resposta NÃO for vazia e o "modo
+    # password" for habilitado ou se a resposta for vazia e o "modo timeout" for
+    # habilitado ou se a resposta for vazia e nem o "modo password" e nem o "modo
+    # timeout" estiverem habilitados. Evita efeitos colaterais graficos indesejados
+    # gerados pelo comando "read" nestas circunstâncias. By Questor
+    # [Ref.: https://stackoverflow.com/a/3980904/3223785 ]
+    if ( [ ${IS_PWD_P} -eq 1 ] && [ -n "$F_EZ_READ_R" ] ) || \
+            ( [ -n "$WAIT_UNTIL_P" ] && [ -z "$F_EZ_READ_R" ] ) ; then
+        echo ""
+    elif [ -z "$F_EZ_READ_R" ] ; then
+        echo ""
+    fi
+
+    # NOTE: Se a resposta for vazia e o "timeout" for habilitado, então a resposta
+    # padrão em "WAIT_UNTIL_RTN_P" será retornada. By Questor
+    if [ -n "$WAIT_UNTIL_P" ] && [ -z "$F_EZ_READ_R" ] ; then
+        F_EZ_READ_R=$WAIT_UNTIL_RTN_P
+    fi
+
 }
 
 GET_USR_INPUT_R=""
@@ -119,13 +198,11 @@ f_get_usr_input() {
     GET_USR_INPUT_R=""
     f_print_long_str "$QUESTION_P"
     if [ ${IS_PWD_P} -eq 0 ] ; then
-        read -e -r -p " (use enter to confirm): " RESP_V
+        f_ez_read " (use enter to confirm): "
+        RESP_V=$F_EZ_READ_R
     else
-
-        # [Ref.: https://stackoverflow.com/a/3980904/3223785 ]
-        read -s -e -r -p " (use enter to confirm): " RESP_V
-        echo ""
-
+        f_ez_read " (use enter to confirm): " 1
+        RESP_V=$F_EZ_READ_R
     fi
     if [ -n "$RESP_V" ] ; then
         GET_USR_INPUT_R="$RESP_V"
@@ -172,7 +249,7 @@ f_get_usr_input_mult() {
             POSSIBLE_OPT=$POSSIBLE_OPT${OPT_ARR_P[$i]}" - "
         else
             # "odd"
-            if (( i <= $(( TOTAL_0 -2 )) )) ; then
+            if (( i <= $(( TOTAL_0 - 2 )) )) ; then
                 USE_PIPE=" | "
             else
                 USE_PIPE=""
@@ -183,15 +260,16 @@ f_get_usr_input_mult() {
     POSSIBLE_OPT=$POSSIBLE_OPT")"
     GET_USR_INPUT_MULT_R=""
     GET_USR_INPUT_MULT_V_R=""
-    read -e -r -p "$QUESTION_P 
-$POSSIBLE_OPT: " RESP_V
+
+    f_ez_read "$QUESTION_P 
+$POSSIBLE_OPT: "
+    RESP_V=$F_EZ_READ_R
+
     if [ -n "$RESP_V" ] ; then
         for (( o=0; o<=$(( $TOTAL_0 -1 )); o++ )) ; do
             if [ $((i%2)) -eq 0 ] && [ "$RESP_V" == "${OPT_ARR_P[$o]}" ] ; then
                 # "even"
-                # GET_USR_INPUT_MULT_R="$RESP_V"
                 GET_USR_INPUT_MULT_R="${OPT_ARR_P[$o]}"
-                # f=$[$f+1]
                 GET_USR_INPUT_MULT_V_R="${OPT_ARR_P[$o+1]}"
                 break
             fi
@@ -201,6 +279,77 @@ $POSSIBLE_OPT: " RESP_V
         fi
     elif [ ${ALLOW_EMPTY_P} -eq 0 ] ; then
         f_get_usr_input_mult "$QUESTION_P" OPT_ARR_P[@] 0
+    fi
+}
+
+F_POWER_SED_ECP_R=""
+f_power_sed_ecp() {
+    : 'Escape strings for the "sed" command.
+
+    Args:
+        F_VAL_TO_ECP (str): Value to be escaped.
+        F_ECP_TYPE (int): 0 - For the TARGET value; 1 - For the REPLACE value.
+
+    Returns:
+        F_POWER_SED_ECP_R (str): Escaped value.
+    '
+
+    F_VAL_TO_ECP=$1
+    F_ECP_TYPE=$2
+    if [ ${F_ECP_TYPE} -eq 0 ] ; then
+    # NOTE: For the TARGET value. By Questor
+
+        F_POWER_SED_ECP_R=$(echo "x${F_VAL_TO_ECP}x" | sed 's/[]\/$*.^|[]/\\&/g' | sed 's/\t/\\t/g' | sed "s/'/\\\x27/g")
+    else
+    # NOTE: For the REPLACE value. By Questor
+
+        F_POWER_SED_ECP_R=$(echo "x${F_VAL_TO_ECP}x" | sed 's/[]\/$*.^|[]/\\&/g' | sed 's/\t/\\t/g' | sed "s/'/\\\x27/g" | sed ':a;N;$!ba;s/\n/\\n/g')
+    fi
+
+    F_POWER_SED_ECP_R=${F_POWER_SED_ECP_R%?}
+    F_POWER_SED_ECP_R=${F_POWER_SED_ECP_R#?}
+}
+
+F_POWER_SED_R=""
+f_power_sed() {
+    : 'Facilitate the use of the "sed" command. Replaces in files and strings.
+
+    Args:
+        F_TARGET (str): Value to be replaced by the value of F_REPLACE.
+        F_REPLACE (str): Value that will replace F_TARGET.
+        F_FILE (Optional[str]): File in which the replacement will be made.
+        F_SOURCE (Optional[str]): String to be manipulated in case "F_FILE" was 
+not informed.
+        F_ALL_OCCUR (Optional[int]): 0 - Replace only on the first occurrence; 
+1 - Replace every occurrence. Default 0.
+
+    Returns:
+        F_POWER_SED_R (str): Return if "F_FILE" is not informed.
+    '
+
+    F_TARGET=$1
+    F_REPLACE=$2
+    F_FILE=$3
+    F_SOURCE=$4
+    ALL_OCCUR=$5
+    if [ -z "$F_ALL_OCCUR" ] ; then
+        F_ALL_OCCUR=0
+    fi
+    f_power_sed_ecp "$F_TARGET" 0
+    F_TARGET=$F_POWER_SED_ECP_R
+    f_power_sed_ecp "$F_REPLACE" 1
+    F_REPLACE=$F_POWER_SED_ECP_R
+    if [ ${F_ALL_OCCUR} -eq 0 ] ; then
+        SED_RPL="'0,/$F_TARGET/s//$F_REPLACE/g'"
+    else
+        SED_RPL="'s/$F_TARGET/$F_REPLACE/g'"
+    fi
+    if [ -z "$F_FILE" ] ; then
+        F_EZ_SED_R=$(echo "x${F_SOURCE}x" | eval "sed $SED_RPL")
+        F_POWER_SED_R=${F_POWER_SED_R%?}
+        F_POWER_SED_R=${F_POWER_SED_R#?}
+    else
+        eval "sed -i $SED_RPL $F_FILE"
     fi
 }
 
@@ -270,6 +419,7 @@ f_ez_sed_ecp() {
     fi
 }
 
+F_EZ_SED_R=""
 f_ez_sed() {
     : 'Facilitar o uso da funcionalidade "sed". Faz replace em arquivos e em 
     strings.
@@ -533,6 +683,16 @@ f_pack_is_inst() {
         else
             F_PACK_IS_INST_R=0
         fi
+
+        # NOTE: O commando "sleep 0.5" tem o objetivo de evitar a falhas como estas
+        # que estão ocorrendo no Ubuntu Server...
+        # "
+        # E: Could not get lock /var/lib/dpkg/lock-frontend. It is held by process 2695 (unattended-upgr)
+        # N: Be aware that removing the lock file is not a solution and may break your system.
+        # E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?
+        # "
+        sleep 0.5
+
     elif [ "$PACK_MANAG" == "zypper" ] ; then
         if zypper se -i --match-word "$PACKAGE_NM_P" > /dev/null 2>&1; then
             if [ ${SKIP_MSG_P} -eq 0 ] && [ ! -z "$EXIST_MSG_P" ] ; then
@@ -759,30 +919,45 @@ f_get_stderr_stdout() {
 
 f_log_manager() {
     : 'Generate and manage output and error logs.
+    TIP: If you want to create a new log file, make sure to delete one that may 
+    already exist, otherwise the function will update the existing log.
 
     Args:
         VALUE_TO_INSERT (str): Value to insert into log file.
-        LOG_TYPE (Optional[int]): 0 - Output log; 1 - Error log. Default 0.
-        CREATE_NEW_LOG (Optional[int]): 0 - Inserts into existing log file; 
-    1 - Creates a new log file. Default 0.
+        LOG_TYPE (Optional[int]): 0 - Output log name; 1 - Error log name; OTHER - 
+    The "OTHER" value will be used as a custom log name. NOTE: In that case "LOG_MODE" 
+    and "PATH_TO_LOG" options will be ignored (use default values for then). "LOG_TYPE=OTHER" 
+    take precedence over "LOG_MODE=OTHER"; Default 0.
+        LOG_MODE (Optional[int]): 0 - Creates (or append to if it already exists) 
+    a log file; 1 - Creates a log with a suffix as part of its name (e.g. 
+    "output-D2020-01-29-T17-50-40.log"); OTHER - The "OTHER" value will be used as 
+    a custom log prefix name plus a suffix (e.g. "my_prefix-D2020-01-29-T17-50-40.log"). 
+    NOTE: In that case "LOG_TYPE" options will be ignored (use default value for
+    it); Default 0.
         PATH_TO_LOG (Optional[str]): Folder path to create log file (without "/" 
-    at the end). If empty, a new log file will be created in the current folder 
-    (value in "EZ_I_DIR_V").
-        VAL_INS_ON_SCREEN (Optional[int]): 0 - Will not print "VALUE_TO_INSERT" 
-    on screen; 1 - Will print "VALUE_TO_INSERT" on screen. Default 0.
+    at the end). If empty will use the current folder path (value in "EZ_I_DIR_V").
+        VAL_INS_ON_SCREEN (Optional[int]): 0 - Will NOT print "VALUE_TO_INSERT" 
+    on screen; 1 - Will PRINT "VALUE_TO_INSERT" on screen. Default 0.
+
+    Returns:
+        F_LOG_MANAGER_R (str): It will return the name and path of the current log. 
+        Useful when we want to update the same log several times and when we used 
+        "LOG_MODE=1" or "LOG_MODE=OTHER" option on the first call to the function.
     '
 
     VALUE_TO_INSERT=$1
     LOG_TYPE=$2
-    CREATE_NEW_LOG=$3
+    LOG_MODE=$3
     PATH_TO_LOG=$4
     VAL_INS_ON_SCREEN=$5
 
     if [ -z "$LOG_TYPE" ] ; then
         LOG_TYPE=0
+    elif [[ "$LOG_TYPE" != "0" ]] && [[ "$LOG_TYPE" != "1" ]] ; then
+        LOG_MODE=0
     fi
-    if [ -z "$CREATE_NEW_LOG" ] ; then
-        CREATE_NEW_LOG=0
+    if [ -z "$LOG_MODE" ] ; then
+        LOG_MODE=0
     fi
     if [ -z "$PATH_TO_LOG" ] ; then
         PATH_TO_LOG="$EZ_I_DIR_V"
@@ -791,23 +966,36 @@ f_log_manager() {
         VAL_INS_ON_SCREEN=0
     fi
 
+    LOG_SUFFIX=""
     LOG_FILE_NAME=""
-    case $LOG_TYPE in
+    case $LOG_MODE in
         0)
-            LOG_FILE_NAME="$PATH_TO_LOG/output.log"
+            case $LOG_TYPE in
+                0)
+                    LOG_FILE_NAME="$PATH_TO_LOG/output.log"
+                ;;
+                1)
+                    LOG_FILE_NAME="$PATH_TO_LOG/error.log"
+                ;;
+                *)
+                    LOG_FILE_NAME="$LOG_TYPE"
+                ;;
+            esac
         ;;
         1)
-            LOG_FILE_NAME="$PATH_TO_LOG/error.log"
+            LOG_SUFFIX=$(date +"-D%Y-%m-%d-T%H-%M-%S")
+            case $LOG_TYPE in
+                0)
+                    LOG_FILE_NAME="$PATH_TO_LOG/output$LOG_SUFFIX.log"
+                ;;
+                1)
+                    LOG_FILE_NAME="$PATH_TO_LOG/error$LOG_SUFFIX.log"
+                ;;
+            esac
         ;;
-    esac
-
-    LOG_APPEND_OR_CREATE=""
-    case $CREATE_NEW_LOG in
-        0)
-            LOG_APPEND_OR_CREATE=">>"
-        ;;
-        1)
-            LOG_APPEND_OR_CREATE=">"
+        *)
+            LOG_SUFFIX=$(date +"-D%Y-%m-%d-T%H-%M-%S")
+            LOG_FILE_NAME="$PATH_TO_LOG/$LOG_MODE$LOG_SUFFIX.log"
         ;;
     esac
 
@@ -815,7 +1003,11 @@ f_log_manager() {
         echo "$VALUE_TO_INSERT"
     fi
 
-    eval "echo \"$VALUE_TO_INSERT\" $LOG_APPEND_OR_CREATE $LOG_FILE_NAME"
+    # [Ref.: https://www.tutorialkart.com/bash-shell-scripting/bash-date-format-options-examples/ ]
+    # eval "echo \"$(date +"D%Y-%m-%d-T%H-%M-%S-%N") - $VALUE_TO_INSERT\" >> $LOG_FILE_NAME"
+    echo "$(date +"D%Y-%m-%d-T%H-%M-%S-%N") - $VALUE_TO_INSERT" >> $LOG_FILE_NAME
+
+    F_LOG_MANAGER_R="$LOG_FILE_NAME"
 }
 
 YES_NO_R=0
@@ -845,22 +1037,20 @@ f_yes_no() {
         WAIT_UNTIL_RTN_P=1
     fi
     if [ -z "$WAIT_UNTIL_P" ] ; then
-        read -e -r -p "$QUESTION_P (y/n) " RESP_V
+        f_ez_read "$QUESTION_P (y/n) "
+        RESP_V=$F_EZ_READ_R
     else
         if [ ${WAIT_UNTIL_RTN_P} -eq 1 ] ; then
             AUT_ANSWER="y"
         elif [ ${WAIT_UNTIL_RTN_P} -eq 0 ] ; then
             AUT_ANSWER="n"
         fi
-
-        # NOTE: O "|| echo \"\"" serve par dar uma quebra de linha se nenhuma
-        # resposta foi informada! By Questor
-        eval "read -e -t$WAIT_UNTIL_P -r -p \"$QUESTION_P (y/n) (\"$AUT_ANSWER\" in $WAIT_UNTIL_P seconds) \" RESP_V" || echo ""
-
+        f_ez_read "$QUESTION_P (y/n)" 0 $WAIT_UNTIL_P "$AUT_ANSWER"
+        RESP_V=$F_EZ_READ_R
     fi
-    if [[ $RESP_V =~ ^([sS]|[yY])$ ]] || ( [ ${WAIT_UNTIL_RTN_P} -eq 1 ] && [ -z "$RESP_V" ] && [ -n "$WAIT_UNTIL_P" ] ) ; then
+    if [[ $RESP_V =~ ^([sS]|[yY])$ ]] ; then
         YES_NO_R=1
-    elif [[ $RESP_V =~ ^([nN])$ ]] || ( [ ${WAIT_UNTIL_RTN_P} -eq 0 ] && [ -z "$RESP_V" ] && [ -n "$WAIT_UNTIL_P" ] ) ; then
+    elif [[ $RESP_V =~ ^([nN])$ ]] ; then
         if [ -n "$RESP_V" ] ; then
             echo "NO!"
         fi
@@ -875,7 +1065,7 @@ F_BAK_MD_R=0
 f_ez_mv_bak() {
     : 'Modifica o nome de um arquivo ou pasta para um nome de backup.
 
-    Adiciona um sufixo ao nome no formato: "-D%Y-%m-%d-T%H-%M-%S.bak".
+    Adiciona um sufixo ao nome no formato: "-D%Y-%m-%d-T%H-%M-%S_BAK".
 
     Args:
         TARGET (str): Caminho para o arquivo ou pasta alvo.
@@ -1354,9 +1544,8 @@ f_is_root() {
     : 'Checar se o usuário é root.
 
     Args:
-        CHK_ONLY_P (Optional[int]): 1 - Apenas verifica e retorna o 
-    resultado; 0 - Se não for root emite erro e encerra a execução. 
-    Padrão 0.
+        CHK_ONLY_P (Optional[int]): 0 - Se não for root emite erro e encerra a execução; 
+    1 - Apenas verifica e retorna o resultado. Padrão 0.
 
     Returns:
         F_IS_ROOT_R (int): 1 - É root; 0 - Não é root.
@@ -1369,10 +1558,9 @@ f_is_root() {
 
     F_IS_ROOT_R=1
     if [[ $EUID -ne 0 ]]; then
-        f_enter_to_cont "ERROR! You need to be root!"
         F_IS_ROOT_R=0
         if [ ${CHK_ONLY_P} -eq 0 ] ; then
-            f_error_exit
+            f_error_exit "You need to be root!"
         fi
     fi
 }
@@ -1613,8 +1801,8 @@ f_get_percent_from() {
         REM_FLOAT_POINT_P=1
     fi
 
-    # NOTA: A estratégia abaixo foi utilizada pq o bash por padrão não 
-    # permite cálculo de ponto flutuante! By Questor
+    # NOTA: A estratégia abaixo foi utilizada porque o bash por padrão não permite
+    # cálculo de ponto flutuante! By Questor
     F_GET_PERCENT_FROM_R=$(awk '{printf("%.5f\n",($1*($2/100)))}' <<<" $VAL_GET_PERCENT_P $PERCENT_VAL_P ")
 
     F_GET_PERCENT_FROM_R=${F_GET_PERCENT_FROM_R}
@@ -1876,6 +2064,7 @@ f_sub_section() {
 # APRESENTAÇÃO!
 # --------------------------------------
 
+F_BEGIN_R=0
 f_begin() {
     : 'Printar uma abertura/apresentação para o instalador do produto.
 
@@ -1887,6 +2076,11 @@ f_begin() {
         ABOUT_P (str): Sobre o produto.
         WARNINGS_P (str): Avisos antes de continuar.
         COMPANY_P (str): Informações sobre a empresa.
+
+    Returns:
+        F_BEGIN_R (int): 0 - If is NOT a string that needs be paged; 1 - If is a 
+    string that needs be paged. NOTE: Useful to control the execution of your script 
+    and allow you control the flow of printed information on terminal.
     '
 
     clear
@@ -1905,7 +2099,10 @@ f_begin() {
     echo "$COMPANY_P"
     f_close_section)
     f_print_long_str "$BEGIN_STR"
-    clear
+    if [ ${F_PRINT_LONG_STR_R} -eq 1 ] ; then
+        clear
+    fi
+    F_BEGIN_R=$F_PRINT_LONG_STR_R
 }
 
 f_end() {
@@ -1938,6 +2135,7 @@ f_terms_licen() {
         TERMS_LICEN_P (str): Termos de licença/uso do produto.
     '
 
+    clear
     if [ ${EZ_I_SKIP_ON_V} -eq 1 ] ; then
         return 0
     fi
@@ -1957,14 +2155,20 @@ f_terms_licen() {
     if [ ${YES_NO_R} -eq 0 ] ; then
         exit 0
     fi
-    clear
+
 }
 
+F_INSTRUCT_R=0
 f_instruct() {
     : 'Printar instruções sobre o produto.
 
     Args:
         INSTRUCT_P (str): Instruções sobre o produto.
+
+    Returns:
+        F_INSTRUCT_R (int): 0 - If is NOT a string that needs be paged; 1 - If is a 
+    string that needs be paged. NOTE: Useful to control the execution of your script 
+    and allow you control the flow of printed information on terminal.
     '
 
     if [ ${EZ_I_SKIP_ON_V} -eq 1 ] ; then
@@ -1977,6 +2181,10 @@ f_instruct() {
     f_close_section)
     clear
     f_print_long_str "$INSTRUCT_STR"
+    if [ ${F_PRINT_LONG_STR_R} -eq 1 ] ; then
+        clear
+    fi
+    F_INSTRUCT_R=$F_PRINT_LONG_STR_R
 }
 
 # < --------------------------------------------------------------------------
